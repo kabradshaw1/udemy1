0c4715df97ae9b171e378e0ca6860f9b77a84c57
kabradshaw1 <itllbok82@gmail.com>
2023-05-24 12:21:45 -0400

    working on tutorial

--- admin/users/views.py
+++ admin/users/views.py
@@ -1,6 +1,20 @@
+from rest_framework import exceptions
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import User
+from .serializers import UserSerializer
+
+@api_view(['GET'])
+def register(request):
+  data = request.data
+
+  if data['password'] != data['password_confirm']:
+    raise exceptions.APIException('Passwords do not match.')
+
+  serializer = UserSerializer(data=data)
+  serializer.is_valid(raise_exception=True)
+  serializer.save()
+  return Response(serializer.data)
 
 @api_view(['GET'])
 def user(request):
